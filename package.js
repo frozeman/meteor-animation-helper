174b69145c30fa193f33abb2be43407c0338ff94
Fabian Vogelsteller <fabian@frozeman.de>
2014-07-29 15:11:27 +0200

    add velocity third party package

--- package.js
+++ package.js
@@ -9,6 +9,8 @@ Package.on_use(function (api) {
     api.use('templating', 'client');
     api.use('jquery', 'client');
 
+    api.use('velocityjs', 'client');
+
     // FILES
     api.add_files('animation-helper.html', 'client');
     api.add_files('animation-helper.js', 'client');
